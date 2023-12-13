fe0b3259ecde98fb44754620fa085c9326f69b52
majnioui <majnioui@gmail.com>
2023-12-13 17:56:25 +0100

    DRY and clean code to not store dead tokens

--- src/main/java/com/monolithicauthtest/app/repository/GitrepRepository.java
+++ src/main/java/com/monolithicauthtest/app/repository/GitrepRepository.java
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GitrepRepository extends JpaRepository<Gitrep, Long> {
     Optional<Gitrep> findFirstByOrderByCreatedAtDesc();
+    void deleteByClientid(String clientid);
 }
